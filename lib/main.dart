6072a07aa7713a0f02a0690e4a393a8524803065
Mahmoud <mahmoudhany374@gmail.com>
2025-01-30 04:31:30 +0200

    created the project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Tal3a());
+}
+
+class Tal3a extends StatelessWidget {
+  const Tal3a({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
